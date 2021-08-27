a0e7ad9fd9bf482016aaa14f32a61be2cf75be16
Shu-Han Hsieh <little.sandraa@gmail.com>
2021-08-27 23:48:47 +0800

    new

--- member_info3new.php
+++ member_info3new.php
@@ -30,7 +30,7 @@
     <link rel="stylesheet" href="CSS/1.header.css">
     <link rel="stylesheet" href="CSS/2.footer.css">
     <link rel="stylesheet" href="CSS/3.btn_to_top.css">
-    <link rel="stylesheet" href="CSS/member_info3.css">
+    <link rel="stylesheet" href="./CSS/member_info3new.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- JS link ----------------------------------->
